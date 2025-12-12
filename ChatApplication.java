92b080add05ff719bbe62e62a78678ca04c28714
T-Hemanaadhan <hemanaadhant@gmail.com>
2025-12-12 23:44:29 +0530

    chat is started

--- ChatApplication.java
+++ ChatApplication.java
@@ -3,5 +3,18 @@ public class ChatApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to the Chat Application!");
         // Initialize chat components here
+
+        publicChat();
+        System.out.println("Chat Application is running...");
+
+        // Start the chat application
+        ChatServer server = new ChatServer();
+        server.start(); 
+          
+    } 
+
     }
-}
+
+
+
+           
