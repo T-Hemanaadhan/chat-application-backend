1d636636cfb14606d9fd35819a21d7902461a6e7
T-Hemanaadhan <hemanaadhant@gmail.com>
2025-12-12 23:29:02 +0530

    chat application

---
+++ ChatApplication.java
@@ -0,0 +1,7 @@
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Chat Application!");
+        // Initialize chat components here
+    }
+}
